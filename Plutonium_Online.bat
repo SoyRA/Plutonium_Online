61cfdf6e6cc3807ade1d1ad261241582481a0de5
Soy R.A. <36781536+SoyRA@users.noreply.github.com>
2025-07-10 23:11:28 -0300

    Initial commit

---
+++ Plutonium_Online.bat
@@ -0,0 +1,189 @@
+:: ============================================================================
+:: @title        Plutonium Online Launcher
+:: @description  Permite actualizar Plutonium y acceder directamente a su lanzador.
+:: @author       SoyRA
+:: @encoding     UTF-8
+:: @platform     Windows (CMD / Batch)
+:: ============================================================================
+
+:: ============================================================================
+:: @section      Configuración Inicial
+:: @description  Configura entorno, codificación, color y título de ventana.
+:: ============================================================================
+
+@ECHO OFF
+CHCP 65001 > NUL
+COLOR 07
+TITLE Plutonium Online Launcher
+SETLOCAL ENABLEEXTENSIONS
+CD /D "%~dp0" || PAUSE && EXIT /B
+
+:: ============================================================================
+:: @section      Variables internas
+:: @description  Variables que el usuario no debe modificar.
+:: ============================================================================
+
+SET PLUTONIUM_UPDATER_URL=https://github.com/mxve/plutonium-updater.rs/releases/download/v0.4.5/plutonium-updater-x86_64-pc-windows-msvc.zip
+SET PLUTONIUM_UPDATER_ARCHIVE=plutonium-updater.zip
+SET PLUTONIUM_UPDATER_FILE=plutonium-updater.exe
+
+SET PLUTONIUM_PATH=%LOCALAPPDATA%\Plutonium
+SET PLUTONIUM_FILE=plutonium-launcher-win32.exe
+
+:: ============================================================================
+:: @section      Flujo principal
+:: @description  Llama a subrutinas principales en orden lógico.
+:: ============================================================================
+
+CALL :show_main_menu
+EXIT /B
+
+:: ============================================================================
+:: @subroutine   show_main_menu
+:: @description  Muestra un menú para seleccionar entre las diferentes opciones.
+:: ============================================================================
+
+:show_main_menu
+    CLS
+    COLOR 07
+    ECHO.
+    ECHO ////////////////////////////////////////////////////////////////
+    ECHO ////                    Plutonium Online                    ////
+    ECHO ////////////////////////////////////////////////////////////////
+    ECHO.
+    ECHO [1] Iniciar el actualizador
+    ECHO [2] Iniciar el lanzador
+    ECHO [3] Instalar redistribuibles
+    ECHO.
+    CHOICE /C 123 /N /M "¿Qué querés hacer? "
+
+    IF %ERRORLEVEL% EQU 1 (
+        CALL :run_updater
+    )
+
+    IF %ERRORLEVEL% EQU 2 (
+        CALL :run_launcher
+    )
+
+    IF %ERRORLEVEL% EQU 3 (
+        CALL :install_redist
+    )
+
+    GOTO :EOF
+
+:: ============================================================================
+:: @subroutine   run_updater
+:: @description  Descarga e inicia el Plutonium CLI Updater.
+:: @author       mxve <https://github.com/mxve/plutonium-updater.rs>
+:: ============================================================================
+
+:run_updater
+    TITLE Plutonium Updater
+    CLS
+    COLOR 07
+
+    IF NOT EXIST "%PLUTONIUM_PATH%\%PLUTONIUM_UPDATER_FILE%" (
+        ECHO Descargando %PLUTONIUM_UPDATER_ARCHIVE% en %PLUTONIUM_PATH%
+        MKDIR "%PLUTONIUM_PATH%" > NUL 2>&1
+        CALL curl.exe -f -L -o "%PLUTONIUM_PATH%\%PLUTONIUM_UPDATER_ARCHIVE%" -# "%PLUTONIUM_UPDATER_URL%"
+
+        IF %ERRORLEVEL% NEQ 0 (
+            COLOR 04
+            ECHO.
+            ECHO Descarga fallida.
+            ECHO Se detendrá la ejecución...
+            ECHO.
+            PAUSE
+            EXIT
+        )
+
+        ECHO Extrayendo %PLUTONIUM_UPDATER_ARCHIVE%...
+        CALL tar -x -f "%PLUTONIUM_PATH%\%PLUTONIUM_UPDATER_ARCHIVE%" -C "%PLUTONIUM_PATH%"
+
+        IF %ERRORLEVEL% NEQ 0 (
+            COLOR 04
+            ECHO.
+            ECHO Extracción fallida.
+            ECHO Se detendrá la ejecución...
+            ECHO.
+            PAUSE
+            EXIT
+        )
+
+        ECHO Eliminando %PLUTONIUM_UPDATER_ARCHIVE%...
+        DEL /F /Q "%PLUTONIUM_PATH%\%PLUTONIUM_UPDATER_ARCHIVE%"
+    )
+
+    ECHO Iniciando %PLUTONIUM_UPDATER_FILE%...
+    CALL "%PLUTONIUM_PATH%\%PLUTONIUM_UPDATER_FILE%" -d "%PLUTONIUM_PATH%" -f -l -q
+
+    IF %ERRORLEVEL% NEQ 0 (
+        COLOR 04
+        ECHO.
+        ECHO Actualización fallida.
+        ECHO Se detendrá la ejecución...
+        ECHO.
+        PAUSE
+        EXIT
+    )
+
+    ECHO.
+    ECHO Actualización exitosa.
+    ECHO.
+    PAUSE
+    CALL :run_launcher
+    GOTO :EOF
+
+:: ============================================================================
+:: @subroutine   run_launcher
+:: @description  Inicia el Plutonium Launcher.
+:: ============================================================================
+
+:run_launcher
+    TITLE Plutonium Launcher
+    CLS
+
+    IF NOT EXIST "%PLUTONIUM_PATH%\bin\%PLUTONIUM_FILE%" (
+        COLOR 04
+        ECHO.
+        ECHO No se encontró %PLUTONIUM_FILE% ¿Cómo querés jugar en Plutonium si nunca lo instalaste?
+        ECHO Se detendrá la ejecución...
+        ECHO.
+        PAUSE
+        EXIT
+    )
+
+    COLOR 02
+    ECHO.
+    ECHO Iniciando el lanzador...
+    START "Plutonium Launcher" /D "%PLUTONIUM_PATH%" "bin\%PLUTONIUM_FILE%"
+    ECHO Ya no necesitás esta ventana, así que se cerrará automáticamente.
+    ECHO.
+    TIMEOUT /T 15
+    GOTO :EOF
+
+
+:: ============================================================================
+:: @subroutine   install_redist
+:: @description  Descarga e instala los programas redistribuibles comunes.
+:: @author       Chase <https://git.chse.sh/chase/redist-installer>
+:: ============================================================================
+
+:install_redist
+    TITLE Redist Installer
+    CLS
+    COLOR 06
+    ECHO Se ejecutará una ventana PowerShell la cual descargará y ejecutará el script de Chase.
+    ECHO Esto es algo que tenés que hacer una sola vez y sólo reinicia la PC al finalizar (no durante la instalación).
+    ECHO.
+    CHOICE /C SN /N /M "¿Querés continuar, (S)í o (N)o? "
+
+    IF %ERRORLEVEL% EQU 1 (
+        CLS
+        COLOR 07
+        ECHO Descargando y ejecutando el script de Chase...
+        CALL PowerShell.exe -NoLogo -NoProfile -NonInteractive -Command "irm chse.dev/ri | iex"
+        PAUSE
+    )
+
+    GOTO :EOF
---
+++ README.md
@@ -0,0 +1,18 @@
+# Plutonium Online
+Este script te permite actualizar **Plutonium** usando [Plutonium CLI Updater](https://github.com/mxve/plutonium-updater.rs) y acceder directamente al Lanzador sin pasar por `plutonium.exe` (el actualizador oficial).\
+Probado su funcionamiento en Windows 10 (Versión 22H2) y Windows 11 (Versión 24H2), pero debería funcionar sin problemas desde Windows 10 (Versión 1803) en adelante.
+
+## Instrucciones de uso
+1. Descargá y extraé [la última versión](https://github.com/SoyRA/Plutonium_Online/archive/refs/heads/main.zip) en cualquier carpeta.
+2. *(Opcional)* Mové el archivo `Plutonium_Online.bat` a un lugar accesible o creá un acceso directo (ya que lo usarás más de una vez).
+3. Dale doble click para iniciarlo y seguí las instrucciones en pantalla.
+
+## Agradecimientos
+- [mxve](https://mxve.de/) - Porque literalmente es quien hizo el *Plutonium CLI Updater*. :P
+- [Chase](https://chse.sh/) - Por su script para descargar e instalar los *redistribuibles*.
+
+<p align="center">Creado con &#x1F92C; por <strong>SoyRA</strong></p>
+
+## Aviso
+Este script fue creado porque algunos usuarios afirman que usar el actualizador oficial les da error mientras que el de terceros no.\
+Caso contrarío, te tocaría hacer algo como [esto](https://docs.skmedix.pl/troubleshooting/launcher-related#connection-problems) donde parece ser que la única solución es usar *Cloudflare WARP*.
